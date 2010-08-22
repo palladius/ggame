139ec2bc0ae24071c901779c161f3a22c8b53172
Riccardo C <rusko@palladius.it>
2010-08-22 12:46:22 +0200

    better schema for matches, implemented ProgressState into scaffold

--- app/models/games/chess.rb
+++ app/models/games/chess.rb
@@ -1,9 +1,10 @@
-=begin
- Here I should define how this game works without rulesets, so basically:
- layout/chessboard, initial state and so on.
-=end
+
+  # Here I should define how this game works without rulesets, so basically:
+  # layout/chessboard, initial state and so on.
 class Chess # < Game
   
-  
+  # TODO
+  #def self.description
+  #end
   
 end
